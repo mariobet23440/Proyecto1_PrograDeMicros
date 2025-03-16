a0d5e7542f3f0eddcd7ca91d1b9f4a5bba275ace
mariobet23440 <bet23440@uvg.edu.gt>
2025-03-16 12:06:43 -0600

    cambio de logica y optimización
    TABLA MEJORADA

--- ProyectoReloj328P/ProyectoReloj328P/main.asm
+++ ProyectoReloj328P/ProyectoReloj328P/main.asm
@@ -50,34 +50,40 @@
 .equ	TIMER_START2 = 158								; Valor inicial de TIMER2 (100 ms)
 
 // Máquina de estados finitos
-.equ	HOUR_STATE = 0x01
-.equ	DATE_STATE = 0x02
-.equ	ALARM_STATE	= 0x04
+.equ	MINUTE_CHANGE =	0X01							; Estado CambiarMinutos
+.equ	HOUR_CHANGE = 0X02 								; Estado CambiarHoras
+.equ	DAY_CHANGE = 0X04								; Estado CambiarDias
+.equ	MONTH_CHANGE = 0X08								; Estado CambiarMeses
+.equ	ALARM_STATE	= 0X10								; Modo Alarma
+
+// Constantes de Lookup Table
+.equ	T0 = 0b1110111
+.equ	T1 = 0b1000100
+.equ	T2 = 0b1101011
+.equ	T3 = 0b1101101
+.equ	T4 = 0b1011100
+.equ	T5 = 0b0111101
+.equ	T6 = 0b0111111
+.equ	T7 = 0b1100100
+.equ	T8 = 0b1111111
+.equ	T9 = 0b1111100 
 
 // R16 y R17 quedan como registros temporales
 
-// Contadores de unidades y decenas de tiempo
-// IMPORTANTE CONSIDERAR QUE R1 Y R0 SE USAN PARA ALGUNAS INSTRUCCIONES (MUL)
-.def	MINUTE_UNITS = R2
-.def	MINUTE_TENS = R3
-.def	HOUR_UNITS = R4
-.def	HOUR_TENS = R5
-.def	DAY_UNITS = R6									; Decenas de meses
-.def	DAY_TENS = R7									; Decenas de días
-.def	MONTH_UNITS = R8 								; Unidades de meses
-.def	MONTH_TENS = R9									; Decenas de meses
-.def	MINUTE_COUNT = R10
-.def	HOUR_COUNT, R11
+
+// Registros inferiores
+.def	T2_AUX_COUNT = R2
 
 // R16, R17 se utilizan como registros de uso común
 
 // Registros auxiliares
-.def	DAY_COUNT = R18
-.def	MONTH_COUNT = R19
-.def	T2_AUX_COUNT = R20								; Contador auxiliar de TIMER2 (Acceder con MOV)
-.def	OUT_PORTD = R21									; Salida de PORTD
-.def	MUX_SIGNAL = R22								; Salida de PORTC (Multiplexado de señal)
-.def	STATE = R23										; Registro de Estado
+.def	MINUTE_COUNT = R18
+.def	HOUR_COUNT = R19
+.def	DAY_COUNT = R20
+.def	MONTH_COUNT = R21
+.def	OUT_PORTD = R22									; Salida de PORTD
+.def	MUX_SIGNAL = R23								; Salida de PORTC (Multiplexado de señal)
+.def	STATE = R24										; Registro de Estado
 
 // No podemos usar registros más allá del 24 porque son los punteros X, Y y Z
 
@@ -88,7 +94,14 @@
 // Definir la tabla en la memoria FLASH (Números del 1 al 10 en display de 7 segmentos)
 .org	0x500
 TABLA:
-	.db 0b1110111, 0b1000100, 0b1101011, 0b1101101, 0b1011100, 0b0111101, 0b0111111, 0b1100100, 0b1111111, 0b1111100 
+	.db T0, T0, T0, T1, T0, T2, T0, T3, T0, T4, T0, T5, T0, T6, T0, T7, T0, T8, T0, T9
+	.db T1, T0, T1, T1, T1, T2, T1, T3, T1, T4, T1, T5, T1, T6, T1, T7, T1, T8, T1, T9
+	.db T2, T0, T2, T1, T2, T2, T2, T3, T2, T4, T2, T5, T2, T6, T2, T7, T2, T8, T2, T9
+	.db T3, T0, T3, T1, T3, T2, T3, T3, T3, T4, T3, T5, T3, T6, T3, T7, T3, T8, T3, T9
+	.db T4, T0, T4, T1, T4, T2, T4, T3, T4, T4, T4, T5, T4, T6, T4, T7, T4, T8, T4, T9
+	.db T5, T0, T5, T1, T5, T2, T5, T3, T5, T4, T5, T5, T5, T6, T5, T7, T5, T8, T5, T9
+// ¿Quién tiene tiempo para hacer tantas comparaciones? Mejor hagamos una buena lookup table
+// Importante observar que para apuntar a las unidades es necesario usar el doble de un registro
 
 
 // --------------------------------------------------------------------
@@ -155,76 +168,142 @@ START:
 	// - INICIALIZACIÓN DE REGISTROS -
 	CLR		T2_AUX_COUNT
 	LDI		MUX_SIGNAL, 0X01
+	CLR		MINUTE_COUNT
+	CLR		HOUR_COUNT
+	LDI		DAY_COUNT, 1
+	LDI		MONTH_COUNT,1
+	LDI		STATE, 1										; Registro de Estado
 
 // --------------------------------------------------------------------
 // | MAINLOOP														  |
 // --------------------------------------------------------------------
 
 MAINLOOP:
-	CALL	UPDATE_DISPLAYS
+	// Realizar multiplexado de señales a transistores
+	CALL	MULTIPLEXADO
+	OUT		PORTC, R16
+	CALL	MODE_OUTPUT
+	CALL	LOOKUP_TABLE
+
+	// Mostrar número en PORTD
 	RJMP	MAINLOOP
 
 
 // --------------------------------------------------------------------
-// | RUTINAS NO DE INTERRUPCIÓN										  |
+// | RUTINA NO DE INTERRUPCIÓN 1 - MULTIPLEXADO						  |
+// --------------------------------------------------------------------
+// - ACTUALIZAR DISPLAYS -
+MULTIPLEXADO:
+	CPI		MUX_SIGNAL, 0X01
+	BREQ	MUX_DISPLAY4
+	CPI		MUX_SIGNAL, 0X02
+	BREQ	MUX_DISPLAY3
+	CPI		MUX_SIGNAL, 0X04
+	BREQ	MUX_DISPLAY2
+	CPI		MUX_SIGNAL, 0X08
+	BREQ	MUX_DISPLAY1
+
+// ENCENDER DISPLAYS
+MUX_DISPLAY4:
+	LDI		R16, 0X01
+	RET
+
+MUX_DISPLAY3:
+	LDI		R16, 0X02
+	RET
+
+MUX_DISPLAY2:
+	LDI		R16, 0X04
+	RET
+
+MUX_DISPLAY1:
+	LDI		R16, 0X08
+	RET
+
+// --------------------------------------------------------------------
+// | RUTINA NO DE INTERRUPCIÓN 2 - MODE_OUTPUT						  |
+// --------------------------------------------------------------------
+// CONFIGURACION DE MODOS (Escoger salidas)
+MODE_OUTPUT:
+	CPI		MUX_SIGNAL, 0X01
+	BREQ	MODE_DISPLAY43
+	CPI		MUX_SIGNAL, 0X02
+	BREQ	MODE_DISPLAY43
+	CPI		MUX_SIGNAL, 0X04
+	BREQ	MODE_DISPLAY21
+	CPI		MUX_SIGNAL, 0X08
+	BREQ	MODE_DISPLAY21
+
+// Salida a displays 4 y 3
+MODE_DISPLAY43:
+	SBRC	STATE, 0
+	MOV		OUT_PORTD, HOUR_COUNT
+	SBRC	STATE, 1
+	MOV		OUT_PORTD, HOUR_COUNT
+	SBRC	STATE, 2
+	MOV		OUT_PORTD, MONTH_COUNT
+	SBRC	STATE, 3
+	MOV		OUT_PORTD, MONTH_COUNT
+	RET
+
+// Salida a displays 2 y 1
+MODE_DISPLAY21:
+	SBRC	STATE, 1
+	MOV		OUT_PORTD, MINUTE_COUNT
+	SBRC	STATE, 2
+	MOV		OUT_PORTD, MINUTE_COUNT
+	SBRC	STATE, 3
+	MOV		OUT_PORTD, DAY_COUNT
+	SBRC	STATE, 4
+	MOV		OUT_PORTD, DAY_COUNT
+	RET
+
+; Si se debe mostrar la salida en DISPLAYS 3 y 4, mostrar horas y meses
+; Si se debe mostrar la salida en DISPLAYS 2 y 1, mostrar minutos y días
+
+// --------------------------------------------------------------------
+// | RUTINA NO DE INTERRUPCIÓN 2 - LOOKUP_TABLE						  |
 // --------------------------------------------------------------------
 
-// - ACTUALIZAR DISPLAYS -
-UPDATE_DISPLAYS:
-	CPI		MUX_SIGNAL, 0X01
-	BREQ	DISPLAY4
-	CPI		MUX_SIGNAL, 0X02
-	BREQ	DISPLAY3
-	CPI		MUX_SIGNAL, 0X04
-	BREQ	DISPLAY2
-	CPI		MUX_SIGNAL, 0X08
-	BREQ	DISPLAY1
-
-// - MODIFICAR DISPLAYS -
-DISPLAY4:
-	LDI		R16, 0X01
-	OUT		PORTC, R16
-	MOV		OUT_PORTD, HOUR_TENS
-	RJMP	SHOW_NUMBER
-
-DISPLAY3:
-	LDI		R16, 0X02
-	OUT		PORTC, R16
-	MOV		OUT_PORTD, HOUR_UNITS
-	RJMP	SHOW_NUMBER
-
-DISPLAY2:
-	LDI		R16, 0X04
-	OUT		PORTC, R16
-	MOV		OUT_PORTD, MINUTE_TENS
-	RJMP	SHOW_NUMBER
-
-DISPLAY1:
-	LDI		R16, 0X08
-	OUT		PORTC, R16
-	MOV		OUT_PORTD, MINUTE_UNITS
-	RJMP	SHOW_NUMBER
-
-// Sacar el número correspondiente en PORTD
-SHOW_NUMBER:
-	// Guardar bit en displays
+// LOOKUP TABLE
+LOOKUP_TABLE:
+	// Guardar bit 7 de PORTD en el Bit T del SREG
 	IN		R17, PORTD
 	BST		R17, 7
 	
 	// Sacar dato de tabla
 	LDI		ZH, HIGH(TABLA<<1)
 	LDI		ZL, LOW(TABLA<<1)
-	ADD		ZL, OUT_PORTD
+
+	// Obtener dirección de tabla (Duplicando OUT_PORTD)
+	MOV		R16, OUT_PORTD
+	ADD		R16, OUT_PORTD
+
+	// Incrementar puntero Z en 2*OUT_PORTD
+	ADD		ZL, R16
+
+	// Sacar el primer dígitooo
 	LPM		R16, Z
 	
+	// Si MUX_SIGNAL es 0x04 o 0x08 sacar el siguiente número
+	SBRC	MUX_SIGNAL, 1
+	INC		ZL
+	SBRC	MUX_SIGNAL, 3
+	INC		ZL
+	LPM		R16, Z
+
 	// Cargar bit de leds intermitentes
 	BLD		R16, 7
 
 	// Mostrar en PORTD
 	OUT		PORTD, R16
+
 	RET
 
-// - REINICIAR TIMER0 -
+// --------------------------------------------------------------------
+// | RUTINAS NO DE INTERRUPCIÓN 3-5 : REINICIO DE TIMERS			  |
+// --------------------------------------------------------------------
+// Reiniciar Timer0
 RESET_TIMER0:
 	// - PRESCALER Y VALOR INICIAL -
 	LDI     R16, PRESCALER0
@@ -275,15 +354,15 @@ PCINT_ISR:
 	PUSH	R16
 
 	// Decrementar contador con botones
-	SBIC	PINC, PC0
+	SBIC	PINB, PB0
 	JMP		DECREMENTAR_PC
 
 	// Incrementar contador con botones
-	SBIC	PINC, PC1
+	SBIC	PINB, PB1
 	JMP		INCREMENTAR_PC
 
 	// Cambiar Estados
-	SBIC	PINC, PC2
+	SBIC	PINB, PB2
 	JMP		CAMBIAR_ESTADOS
 	
 	// Si no se detecta nada, ir al final
@@ -291,64 +370,42 @@ PCINT_ISR:
 	
 
 CAMBIAR_ESTADOS:
+	// Leer el SREG
+	IN		R16, SREG
+
 	// Cambiar Minutos -> Cambiar Horas
-	CPI		STATE, 1
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 2
+	CPI		STATE, MINUTE_CHANGE
+	SBRC	R16, SREG_Z
+	LDI		STATE, HOUR_CHANGE
 
 	// Cambiar Horas -> Cambiar Dias
-	CPI		STATE, 2
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 3
+	CPI		STATE, HOUR_CHANGE
+	SBRC	R16, SREG_Z
+	LDI		STATE, DAY_CHANGE
 
 	// Cambiar Dias -> Cambiar Meses
-	CPI		STATE, 3
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 4
+	CPI		STATE, DAY_CHANGE
+	SBRC	R16, SREG_Z
+	LDI		STATE, MONTH_CHANGE
 
 	// Cambiar Meses -> Alarma
-	CPI		STATE, 4
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 5
+	CPI		STATE, MONTH_CHANGE
+	SBRC	R16, SREG_Z
+	LDI		STATE, ALARM_STATE
 
 	// Alarma -> Cambiar Minutos
-	CPI		STATE, 5
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 1
+	CPI		STATE, ALARM_STATE
+	SBRC	R16, SREG_Z
+	LDI		STATE, MINUTE_CHANGE
 	
 	// Saltar al final
 	RJMP	END_PC_ISR
 
-DECRMENTAR_PC:
-	// Cambiar Minutos -> Cambiar Horas
-	CPI		STATE, 1
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 2
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 2
+DECREMENTAR_PC:
+	JMP		END_PC_ISR
 
-	// Cambiar Horas -> Cambiar Dias
-	CPI		STATE, 2
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 3
-
-	// Cambiar Dias -> Cambiar Meses
-	CPI		STATE, 3
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 4
-
-	// Cambiar Meses -> Alarma
-	CPI		STATE, 4
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 5
-
-	// Alarma -> Cambiar Minutos
-	CPI		STATE, 5
-	SBIC	SREG, SREG_Z
-	LDI		STATE, 1
-	
-	// Saltar al final
-	RJMP	END_PC_ISR
+INCREMENTAR_PC:
+	JMP		END_PC_ISR
 
 END_PC_ISR:
 	POP		R16
@@ -398,72 +455,28 @@ TIMER1_ISR:
 	// Reiniciar el TIMER1
 	CALL	RESET_TIMER1
 
-	// INCREMENTO DE UNIDADES DE MINUTOS
-	// Incrementar MINUTE_UNITS
-	MOV		R16, MINUTE_UNITS
-	INC		R16
-	MOV		MINUTE_UNITS, R16	
+	// INCREMENTO DE MINUTOS
+	INC		MINUTE_COUNT
 	
-	// INCREMENTO DE DECENAS DE MINUTOS
-	// Si MINUTE_UNITS es mayor o igual a 10, limpiarlo e incrementar MINUTE_TENS
-	CPI		R16, 10
+	// INCREMENTO DE HORAS
+	// Si MINUTE_TENS es mayor o igual a 60, limpiarlo e incrementar HOUR_COUNT
+	CPI		MINUTE_COUNT, 60
 	BRLO	INTERMEDIATE_JUMP1
-	CLR		MINUTE_UNITS
-	MOV		R16, MINUTE_TENS
-	INC		R16
-	MOV		MINUTE_TENS, R16
-	
-	// INCREMENTO DE UNIDADES DE HORAS
-	// Si MINUTE_TENS es mayor o igual a 6, limpiarlo e incrementar HOUR_UNITS
-	CPI		R16, 6
-	BRLO	INTERMEDIATE_JUMP1
-	CLR		MINUTE_TENS
-	MOV		R16, HOUR_UNITS
-	INC		R16
-	MOV		HOUR_UNITS, R16
+	CLR		MINUTE_COUNT
+	INC		HOUR_COUNT
 
-	// INCREMENTO DE DECENAS DE HORAS
-	// Si HOUR_UNITS es mayor o igual a 10, limpiarlo e incrementar HOUR_TENS
-	CPI		R16, 10
+	// INCREMENTO DE DÍAS
+	// Si HOUR_COUNT es mayor o igual a 24, limpiarlo e incrementar DAY_COUNT
+	CPI		HOUR_COUNT, 24
 	BRLO	INTERMEDIATE_JUMP1
-	CLR		HOUR_UNITS
-	MOV		R16, HOUR_TENS
-	INC		R16
-	MOV		HOUR_TENS, R16
+	CLR		HOUR_COUNT
+	INC		DAY_COUNT
 
-	// INCREMENTO DE UNIDADES DE DÍA
-	// Si HOUR_TENS no es igual a 2 terminar subrutina
-	CPI		R16, 2
+	// Si el número de días no excede 28 (El mínimo para cambiar de mes) salir para
+	// evitar comparaciones innecesarias
+	CPI		DAY_COUNT, 28
 	BRLO	INTERMEDIATE_JUMP1
-
-	// Si HOUR_UNITS es menor que 4 terminar subrutina
-	MOV		R16, HOUR_UNITS		; Importante cargar los contenidos de HOUR_UNITS
-	CPI		R16, 4
-	BRLO	INTERMEDIATE_JUMP1
-
-	// Si no, limpiar ambos contadores e incrementar DAY_UNITS
-	CLR		HOUR_UNITS
-	CLR		HOUR_TENS
-	MOV		R16, DAY_UNITS
-	INC		R16
-	MOV		DAY_UNITS, R16
-
-	// INCREMENTO DE DECENAS DE DÍAS
-	CPI		R16, 10
-	BRLO	INTERMEDIATE_JUMP1
-	CLR		DAY_UNITS
-	MOV		R16, DAY_TENS
-	INC		R16
-	MOV		DAY_TENS, R16
-	
-	// Antes de ejecutar todo lo siguiente, determinar si el número de días no excede 28
-	CPI		R16, 2			; Comparar DAY_TENS con 2
-	BRLO	INTERMEDIATE_JUMP1
-	MOV		R16, DAY_UNITS
-	CPI		R16, 8
-	BRLO	INTERMEDIATE_JUMP1
-	// Esto ayudará a evitar las comparaciones para incrementar meses cuando no sean necesarias
-	
+		
 	// Pero, si ese no es el caso
 	// "Goku eta vaina se puso seria"
 	// Si el número de días excede 29, incrementar mes (Muchas comparaciones)
@@ -474,23 +487,7 @@ INTERMEDIATE_JUMP1:
 
 	
 // COMPARACIONES PARA INCREMENTO DE MESES
-COMPARACIONES_MES:
-	// Calcular el número de días usando las decenas y unidades de día
-	MOV		R16, DAY_UNITS		; Cargamos DAY_UNITS a R16
-	ADD		DAY_COUNT, R16		; Sumamos unidades a R18
-	LDI		R16, 10				; Cargamos 10 a R16
-	MUL		R16, DAY_TENS		; Multiplicar DAY_TENS por 10
-	MOV		R16, R0				; El byte bajo de la multiplicación se guarda en R0 (Limitado a 10)
-	ADD		DAY_COUNT, R16		; Ahora R18 contiene el número de días transcurridos
-
-	// Hacemos lo mismo con MONTH_COUNT
-	MOV		R16, MONTH_UNITS	; Cargamos MONTH_UNITS a R16
-	ADD		MONTH_COUNT, R16	; Sumamos unidades a R18
-	LDI		R16, 10				; Cargamos 10 a R16
-	MUL		R16, MONTH_TENS		; Multiplicar MONTH_TENS por 10
-	MOV		R16, R0				; El byte bajo de la multiplicación se guarda en R0 (Limitado a 10)
-	ADD		MONTH_COUNT, R16	; Ahora R18 contiene el número de meses transcurridos
-	
+COMPARACIONES_MES:	
 	// Verificar si el mes es febrero
 	CPI		MONTH_COUNT, 2
 	BREQ	FEBRERO
@@ -509,26 +506,22 @@ COMPARACIONES_MES:
 	RJMP	MESES_31
 
 FEBRERO:
-	// Verificar si el contador pasa de 28
+	// Verificar si el contador de días pasa de 28
 	CPI		DAY_COUNT, 28
     BRLO	END_T1_ISR
 
-	// Si han pasado más de 28 días, reiniciar contadores de días
-    LDI		R16, 1
-	MOV		DAY_UNITS, R16
-	CLR		DAY_TENS
+	// Si han pasado más de 28 días, reiniciar contador de días
+    LDI		DAY_COUNT, 1
     RJMP	AUMENTAR_MES
 
 MESES_30:
-	// Verificar si el contador pasa de 30
+	// Verificar si el contador de días pasa de 30
 	CPI		DAY_COUNT, 30
     BRLO	END_T1_ISR
 
-	// Si han pasado más de 30 días, reiniciar contadores de días
-    LDI		R16, 1
-	MOV		DAY_UNITS, R16
-	CLR		DAY_TENS
-    RJMP	AUMENTAR_MES
+	// Si han pasado más de 30 días, reiniciar contador de días
+    LDI		DAY_COUNT, 1
+	RJMP	AUMENTAR_MES
 
 MESES_31:
 	// Verificar si el contador pasa de 31
@@ -536,10 +529,8 @@ MESES_31:
     BRLO	END_T1_ISR
 
 	// Si han pasado más de 31 días, reiniciar contadores de días
-    LDI		R16, 1
-	MOV		DAY_UNITS, R16
-	CLR		DAY_TENS
-    RJMP	AUMENTAR_MES
+    LDI		DAY_COUNT, 1
+	RJMP	AUMENTAR_MES
 
 
 // AUMENTAR MES
@@ -551,28 +542,12 @@ AUMENTAR_MES:
 
 	// Si las unidades de meses excenden 12, reiniciar ambos contadores
 	CPI		MONTH_COUNT, 12
-	BRLO	END_T1_ISR
-	LDI		R16, 1
-	MOV		MONTH_UNITS, R16
-	CLR		MONTH_TENS
+	LDI		MONTH_COUNT, 1
 	RJMP	END_T1_ISR
 
 // Meses que NO son diciembre
 NO_DICIEMBRE:
-	// Incrementar contador de unidades
-	MOV		R16, MONTH_UNITS
-	INC		R16
-	MOV		MONTH_UNITS, R16
-	CPI		R16, 10
-	BRLO	END_T1_ISR
-
-	// Incrementar contador de decenas
-	CLR		MONTH_UNITS
-	MOV		R16, MONTH_TENS
-	INC		R16
-	MOV		MONTH_TENS, R16
-
-	// No hacer nada
+	INC		MONTH_COUNT
 	BRLO	END_T1_ISR
 
 
@@ -595,14 +570,18 @@ TIMER2_ISR:
 	// Reiniciar el TIMER2
 	CALL	RESET_TIMER2
 
+	
+
 	// Incrementar el contador auxiliar hasta 6
 	INC		T2_AUX_COUNT
-	CPI		T2_AUX_COUNT, 6
+	
+	// Leer T2_AUX_COUNT y comparar
+	MOV		R16, T2_AUX_COUNT 
+	CPI		R16, 6
 	BRLO	END_T2_ISR
 
 	// Si el contador rebasa 6, reiniciar y alternar el bit PD7
 	CLR		T2_AUX_COUNT		; Guardar el reinicio del contador
-
 	IN		R16, PORTD			; Leer estado actual de PORTD
 	LDI		R17, (1 << PD7)
     EOR		R16, R17			; Alternar bit PD7 (D7)
